d325ce396947812013d76b45bb275d33e6211f3e
sas7629 <sas7629@rit.edu>
2020-06-21 19:20:20 -0400

    Authentication feature

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/feed/routes/feed.router.ts
@@ -18,13 +18,39 @@ router.get('/', async (req: Request, res: Response) => {
 
 //@TODO
 //Add an endpoint to GET a specific resource by Primary Key
+router.get('/:id', async(req: Request, res: Response) => {
+    let {id} = req.params;
+    const item = await FeedItem.findByPk(id);
+    if ( !item ) {
+        return res.status(404).send({message: 'id not found'});
+    }
+    item.url = AWS.getGetSignedUrl(item.url);
+    res.status(200).send(item);
+});
 
 // update a specific resource
 router.patch('/:id', 
     requireAuth, 
     async (req: Request, res: Response) => {
         //@TODO try it yourself
-        res.send(500).send("not implemented")
+        let {id} = req.params;
+        const caption = req.body.caption;
+        const fileName = req.body.url;
+        const item = await FeedItem.findByPk(id);
+
+        if ( !item ) {
+            return res.status(404).send({message: 'id not found'});
+        }
+        if (caption) {
+            item.caption = caption;
+        }
+        if (fileName) {
+            item.url = fileName;
+        }
+
+        const saved_item = await item.save();
+        saved_item.url = AWS.getGetSignedUrl(saved_item.url);
+        res.status(201).send(saved_item);
 });
 
 
